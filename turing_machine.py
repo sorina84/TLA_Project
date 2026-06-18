1fcc656f25bf6483edfec185483fb2a0cd22a8eb
sorina84 <afsharysorina@gmail.com>
2026-06-18 18:56:45 +0330

    copelet Turing Machines file

---
+++ turing_machine.py
@@ -0,0 +1,81 @@
+class TuringMachine:
+    def __init__(self, transitions, start_state='q0', accept_state='qa', reject_state='qr', blank_symbol=''):
+        self.transitions = transitions
+        self.start_state = start_state
+        self.accept_state = accept_state
+        self.reject_state = reject_state
+        self.blank_symbol = blank_symbol
+
+    def run(self, input_):
+        tape = list(input_) if input_ else []
+        current_symbol = tape[0] if tape else self.blank_symbol
+        right_hand_side = tape[1:] if len(tape) > 1 else []
+        left_hand_side = []
+        current_state = self.start_state
+
+        while True:
+            if current_state == self.accept_state:
+                action = 'Accept'
+            elif current_state == self.reject_state:
+                action = 'Reject'
+            else:
+                action = None
+
+            config = {
+                'state': current_state,
+                'left_hand_side': left_hand_side,
+                'symbol': current_symbol,
+            }
+            yield (action, config)
+
+            if action is not None:
+                break
+
+            key = (current_state, current_symbol)
+            if key not in self.transitions:
+                current_state = self.reject_state
+                continue
+
+            next_state, write_symbol, direction = self.transitions[key]
+            current_state = next_state
+            current_symbol = write_symbol
+
+            if direction == 'R':
+                left_hand_side.append(write_symbol)
+                if right_hand_side:
+                    current_symbol = right_hand_side.pop(0)
+                else:
+                    current_symbol = self.blank_symbol
+            elif direction == 'L':
+                right_hand_side.insert(0, write_symbol)
+                if left_hand_side:
+                    current_symbol = left_hand_side.pop()
+                else:
+                    current_symbol = self.blank_symbol
+
+    def accepts(self, input_, **kwargs):
+        for action, config in self.run(input_):
+            if action == 'Accept':
+                return True
+            elif action == 'Reject':
+                return False
+        return None
+
+    def rejects(self, input_, **kwargs):
+        result = self.accepts(input_, **kwargs)
+        if result is None:
+            return None
+        return not result
+
+    def debug(self, input_, step_limit=100, colored=False):
+        step = 0
+        for action, config in self.run(input_):
+            if step >= step_limit:
+                print("... (step limit reached)")
+                break
+            left = ''.join(reversed(config['left_hand_side']))
+            sym = config['symbol']
+            print(f"Step {step}: state={config['state']}, tape=...{left}[{sym}]... , action={action}")
+            step += 1
+            if action is not None:
+                break
